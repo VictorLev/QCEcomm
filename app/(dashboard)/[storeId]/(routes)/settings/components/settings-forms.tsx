6d43b1e4b5e4daa6c6fdc0896b45174af8faf341
Victor <levesque.vic@gmail.com>
2023-10-14 20:46:31 +1100

    store Id route

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-forms.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-forms.tsx
@@ -1,14 +1,16 @@
-"user client"
+"use client"
 
 import * as z from "zod"
 import { Store } from "@prisma/client";
 import { Heading } from "@/components/ui/heading"
 import { Button } from "@/components/ui/button";
-import { Trash, TrashIcon } from "lucide-react";
+import { Trash } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -33,7 +35,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         defaultValues: initialData
     })
 
-    
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data)
+    }
 
 
 
@@ -46,14 +50,42 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                     description="Manage store preferences"
                 />
                 <Button
+                    disabled={loading}
                     variant="destructive"
                     size="sm"
-                    //onClick={() => {}}
+                    onClick={() => setOpen(true) }
                 >
                     <Trash className="h-4 w-4"/>
                 </Button>
             </div>
             <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full"> 
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField 
+                            control={form.control}
+                            name="name"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        <FormControl>
+                                            <Input disabled={loading} placeholder="Store name" {...field}/>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormLabel>
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <Button disabled={loading} className="ml-auto" type="submit">
+                        Save Changes
+                    </Button>
+
+
+
+                </form>
+            </Form>
         </>
     )
 }
